27b3fe3fcd28cd3d05a7eb4af179cf1b67019f80
Mattcz2026 <matthewjczaja@lewisu.edu>
2026-05-11 09:55:32 -0500

    Update DogDraw_MC.py

--- DogDraw_MC.py
+++ DogDraw_MC.py
@@ -1,1 +1,46 @@
+print("Start Dog Draw!")
+import Dog 
+screen = dog.Screen()
+screen.setup(width=450, height=450)
+d = dog.dog()
 
+
+davidthedog = dog.dog()
+davidthedog.forward(100)
+
+dog.done()
+spiral = dog.Dog()
+spiral.pencolor("purple")
+for i in range(40):
+    spiral.forward(i * 10)
+    spiral.rght(144)
+  '''
+
+print('Reading a text file line by line.')
+DogDraw = open(TEXTFILENAME, 'r')
+Line = DogDraw.readline()
+
+while line:
+    print(line, end='')
+  line = DogDraw
+
+  if(len(parts) == 3):
+  color = parts[0]
+  x = int(parts[1])
+  y = int(parts[2])
+
+
+  DogDraw.color(color)
+  DogDraw.goto(x,y)
+  DogDraw.pendown()
+
+
+  draw.pendown()
+  draw.goto(x,y)
+
+  Line = DogDraw.readline()
+
+  dog.done()
+
+  #Wait to press the enter key button
+  print('\nEnd')
